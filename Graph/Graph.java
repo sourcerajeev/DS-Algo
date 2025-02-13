450dd8fec1c35ddd2536ee90cefc58826d8f151f
Rajeev <rajeev.work96@gmail.com>
2025-02-13 12:10:56 +0530

    optimized adjacency list

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,27 +1,26 @@
 package Graph;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Graph {
     private int vertices;
-    private List<List<Integer>> adjacencyList;
+    private Map<Integer, List<Integer>> adjacencyList;
 
     public Graph(int vertices) {
         this.vertices = vertices;
-        adjacencyList = new LinkedList<>();
-        for (int i = 0; i < vertices; i++) {
-            adjacencyList.add(new LinkedList<>());
-        }
+        adjacencyList = new HashMap<>();
     }
 
     public void addEdge(int source, int destination) {
-        adjacencyList.get(source).add(destination);
-        adjacencyList.get(destination).add(source);
+        adjacencyList.computeIfAbsent(source, k -> new LinkedList<>()).add(destination);
+        adjacencyList.computeIfAbsent(destination, k -> new LinkedList<>()).add(source);
     }
 
     public void addDirectedEdge(int source, int destination) {
-        adjacencyList.get(source).add(destination);
+        adjacencyList.computeIfAbsent(source, k -> new LinkedList<>()).add(destination);
     }
 
     public List<Integer> getAdjacencyList(int vertex) {
